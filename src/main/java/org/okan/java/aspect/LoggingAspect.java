19a386f2adab65f1bf220d4cb176565601fa134d
Okan Demirtaş <demirtasokan@gmail.com>
2017-04-09 13:41:58 +0300

    added PointCut annotation

--- src/main/java/org/okan/java/aspect/LoggingAspect.java
+++ src/main/java/org/okan/java/aspect/LoggingAspect.java
@@ -2,6 +2,7 @@ package org.okan.java.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 /**
  * Created by okan on 9.04.2017.
@@ -9,9 +10,17 @@ import org.aspectj.lang.annotation.Before;
 @Aspect
 public class LoggingAspect {
 
-  @Before("execution(* org.okan.java.model.*.get*())")
+  @Before("allGetters()")
   public void LoggingAdvice(){
     System.out.println("Advice run. Get Method called.");
   }
+
+  @Before("allGetters()")
+  public void secondAdvice(){
+    System.out.println("Second advice executed.");
+  }
+
+  @Pointcut("execution(* get*())")
+  public void allGetters(){}
 
 }
