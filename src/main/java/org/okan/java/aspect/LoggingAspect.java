57d34e289d0e752f8a85e1a7295819ae3019ffc9
Okan Demirtaş <demirtasokan@gmail.com>
2017-04-09 13:33:09 +0300

    spesific package aspect for get*()

--- src/main/java/org/okan/java/aspect/LoggingAspect.java
+++ src/main/java/org/okan/java/aspect/LoggingAspect.java
@@ -9,7 +9,7 @@ import org.aspectj.lang.annotation.Before;
 @Aspect
 public class LoggingAspect {
 
-  @Before("execution(public * get*())")
+  @Before("execution(* org.okan.java.model.*.get*())")
   public void LoggingAdvice(){
     System.out.println("Advice run. Get Method called.");
   }
